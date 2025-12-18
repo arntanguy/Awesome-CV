6a10f3e1b3c6934c19a55dbb403757cdf086bc1b
Arnaud TANGUY <arn.tanguy@gmail.com>
2025-12-18 15:53:54 +0100

    nix: upgrade to nixos-24.05 to fix fontawesome issues
    It seems that nixos-25.05 does not build the pdf correctly, so stick
    with this version instead

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     # Using a more recent nixpkgs is recommended for better LuaLaTeX support
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
